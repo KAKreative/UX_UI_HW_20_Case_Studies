9eafe2514c372c0f99226c1cab6025d13427879d
Kimmiepoo <104878858+Kimmiepoo@users.noreply.github.com>
2022-11-07 01:38:17 -0500

    Added Case Study Pages

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # UX_UI_HW_20_Case_Studies
+<!-- Added 3 Case Study Pages to Portfolio -->
---
+++ js/index.js
@@ -0,0 +1,4 @@
+// // JQuery Smooth Scrolling
+const scroll = new SmoothScroll('.container a[href*="#"]',{
+speed: 800
+});
